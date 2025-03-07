1e93b12a5743e5ce53aeccf608722289c04ecc68
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-03-07 10:55:15 +0200

    Implement authentication for Profile and Upload pages; add CustomError class and error messages utility

--- src/lib/authActions.ts
+++ src/lib/authActions.ts
@@ -5,6 +5,7 @@ import CustomError from '../classes/CustomError';
 import bcrypt from 'bcryptjs';
 import { TokenContent } from 'hybrid-types/DBTypes';
 import jwt from 'jsonwebtoken';
+import { redirect } from 'next/navigation';
 
 export async function login(formData: FormData) {
   if (!process.env.JWT_SECRET) {
@@ -42,28 +43,46 @@ export async function login(formData: FormData) {
 
 export async function logout() {
   // Destroy the session
-  cookies().set('session', '', { expires: new Date(0) });
+  const cookieStore = await cookies();
+  cookieStore.set('session', '', { expires: new Date(0) });
 }
 
 export async function getSession() {
-  const session = cookies().get('session')?.value;
+  if (!process.env.JWT_SECRET) {
+    throw new CustomError('JWT key not set', 500);
+  }
+  const key = process.env.JWT_SECRET;
+
+  const cookieStore = await cookies();
+  const session = cookieStore.get('session')?.value;
   if (!session) return null;
-  return await decrypt(session);
+  return jwt.verify(session, key) as TokenContent;
 }
 
 export async function updateSession(request: NextRequest) {
+  if (!process.env.JWT_SECRET) {
+    throw new CustomError('JWT key not set', 500);
+  }
+  const key = process.env.JWT_SECRET;
   const session = request.cookies.get('session')?.value;
   if (!session) return;
 
   // Refresh the session so it doesn't expire
-  const parsed = await decrypt(session);
-  parsed.expires = new Date(Date.now() + 10 * 1000);
+  const tokenContent = jwt.verify(session, key) as TokenContent;
+  const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
   const res = NextResponse.next();
   res.cookies.set({
     name: 'session',
-    value: await encrypt(parsed),
+    value: jwt.sign(tokenContent, key, { expiresIn: '7d' }),
     httpOnly: true,
-    expires: parsed.expires,
+    expires,
   });
   return res;
+}
+
+export async function requireAuth() {
+  const session = await getSession();
+  if (!session?.user_id) {
+    redirect('/');
+  }
 }
